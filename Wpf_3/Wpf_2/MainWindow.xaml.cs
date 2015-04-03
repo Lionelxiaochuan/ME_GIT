059547778338a805a2e25131679bc6b94abc939d
unknown <haoang8888@sina.com>
2015-04-04 01:22:11 +0800

    come on! 今天熬夜了，粗略地看完了三个

--- Wpf_3/Wpf_2/MainWindow.xaml.cs
+++ Wpf_3/Wpf_2/MainWindow.xaml.cs
@@ -33,7 +33,10 @@ namespace Wpf_2
         private WriteableBitmap depthImageBitmap;
         private Int32Rect depthImageBitmapRect;
         private int depthImageStride;
-        private byte[] depthImagePixelData;
+        private short[] depthImagePixelData;
+        private short[] depthPixelDate;
+
+        private DepthImageFrame lastDepthFrame;
 
         public KinectSensor Kinect
         {
@@ -86,10 +89,185 @@ namespace Wpf_2
                     short[] depthPixelDate = new short[depthFrame.PixelDataLength];
                     depthFrame.CopyPixelDataTo(depthPixelDate);
                     depthImageBitmap.WritePixels(depthImageBitmapRect, depthPixelDate, depthImageStride, 0);
+                    this.CreateColorDepthImage(depthFrame, depthPixelDate);
                 }
             }
+
+
+            //if (lastDepthFrame != null)
+            //{
+            //    lastDepthFrame.Dispose();
+            //    lastDepthFrame = null;
+            //}
+            //lastDepthFrame = e.OpenDepthImageFrame();
+            //if (lastDepthFrame != null)
+            //{
+            //    depthPixelDate = new short[lastDepthFrame.PixelDataLength];
+            //    lastDepthFrame.CopyPixelDataTo(depthPixelDate);
+            //    depthImageBitmap.WritePixels(depthImageBitmapRect, depthPixelDate, depthImageStride, 0);
+            //}
         }
 
+        //private void DepthImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    Point p = e.GetPosition(DepthImage);
+        //    if (depthPixelDate != null && depthPixelDate.Length > 0)
+        //    {
+        //        Int32 pixelIndex = (Int32)(p.X + ((Int32)p.Y * this.lastDepthFrame.Width));
+        //        Int32 depth = this.depthPixelDate[pixelIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+        //        Int32 depthInches = (Int32)(depth * 0.0393700787);
+        //        Int32 depthFt = depthInches / 12;
+        //        depthInches = depthInches % 12;
+        //        PixelDepth.Text = String.Format("{0}mm~{1}'{2}", depth, depthFt, depthInches);
+        //    }
+        //}
+
+        private void CreateLighterShadesOfGray(DepthImageFrame depthFrame, short[] pixelData)
+        {
+            Int32 depth;
+            Int32 loThreashold = 0;
+            Int32 hiThreshold = 3500;
+            short[] enhPixelData = new short[depthFrame.Width * depthFrame.Height];
+            for (int i = 0; i < pixelData.Length; i++)
+            {
+                depth = pixelData[i] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+                if (depth < loThreashold || depth > hiThreshold)
+                {
+                    enhPixelData[i] = 0xFF;
+                }
+                else
+                {
+                    enhPixelData[i] = (short)~pixelData[i];
+                }
+
+            }
+            EnhancedDepthImage.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height, 96, 96, PixelFormats.Gray16, null, enhPixelData, depthFrame.Width * depthFrame.BytesPerPixel);
+        }
+
+        private void CreateBetterShadesOfGray(DepthImageFrame depthFrame, short[] pixelData)
+        {
+            Int32 depth;
+            Int32 gray;
+            Int32 loThreashold = 0;
+            Int32 bytePerPixel = 4;
+            Int32 hiThreshold = 3500;
+            byte[] enhPixelData = new byte[depthFrame.Width * depthFrame.Height * bytePerPixel];
+            for (int i = 0, j = 0; i < pixelData.Length; i++, j += bytePerPixel)
+            {
+                depth = pixelData[i] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+                if (depth < loThreashold || depth > hiThreshold)
+                {
+                    gray = 0xFF;
+                }
+                else
+                {
+                    gray = (255 * depth / 0xFFF);
+                }
+                enhPixelData[j] = (byte)gray;
+                enhPixelData[j + 1] = (byte)gray;
+                enhPixelData[j + 2] = (byte)gray;
+
+            }
+            EnhancedDepthImage.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height, 96, 96, PixelFormats.Bgr32, null, enhPixelData, depthFrame.Width * bytePerPixel);
+        }
+
+        private void CreateColorDepthImage(DepthImageFrame depthFrame, short[] pixelData)
+        {
+            Int32 depth;
+            Double hue;
+            Int32 loThreshold = 1200;
+            Int32 hiThreshold = 3500;
+            Int32 bytesPerPixel = 4;
+            byte[] rgb = new byte[3];
+            byte[] enhPixelData = new byte[depthFrame.Width * depthFrame.Height * bytesPerPixel];
+
+            for (int i = 0, j = 0; i < pixelData.Length; i++, j += bytesPerPixel)
+            {
+                depth = pixelData[i] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+                if (depth < loThreshold || depth > hiThreshold)
+                {
+                    enhPixelData[j] = 0x00;
+                    enhPixelData[j + 1] = 0x00;
+                    enhPixelData[j + 2] = 0x00;
+                }
+                else
+                {
+                    hue = ((360 * depth / 0xFFF) + loThreshold);
+                    ConvertHslToRgb(hue, 100, 100, rgb);
+
+                    enhPixelData[j] = rgb[2];  //Blue
+                    enhPixelData[j + 1] = rgb[1];  //Green
+                    enhPixelData[j + 2] = rgb[0];  //Red
+                }
+            }
+
+            EnhancedDepthImage.Source = BitmapSource.Create(depthFrame.Width, depthFrame.Height, 96, 96, PixelFormats.Bgr32, null, enhPixelData, depthFrame.Width * bytesPerPixel);
+        }
+
+        public void ConvertHslToRgb(Double hue, Double saturation, Double lightness, byte[] rgb)
+        {
+            Double red = 0.0;
+            Double green = 0.0;
+            Double blue = 0.0;
+            hue = hue % 360.0;
+            saturation = saturation / 100.0;
+            lightness = lightness / 100.0;
+
+            if (saturation == 0.0)
+            {
+                red = lightness;
+                green = lightness;
+                blue = lightness;
+            }
+            else
+            {
+                Double huePrime = hue / 60.0;
+                Int32 x = (Int32)huePrime;
+                Double xPrime = huePrime - (Double)x;
+                Double L0 = lightness * (1.0 - saturation);
+                Double L1 = lightness * (1.0 - (saturation * xPrime));
+                Double L2 = lightness * (1.0 - (saturation * (1.0 - xPrime)));
+
+                switch (x)
+                {
+                    case 0:
+                        red = lightness;
+                        green = L2;
+                        blue = L0;
+                        break;
+                    case 1:
+                        red = L1;
+                        green = lightness;
+                        blue = L0;
+                        break;
+                    case 2:
+                        red = L0;
+                        green = lightness;
+                        blue = L2;
+                        break;
+                    case 3:
+                        red = L0;
+                        green = L1;
+                        blue = lightness;
+                        break;
+                    case 4:
+                        red = L2;
+                        green = L0;
+                        blue = lightness;
+                        break;
+                    case 5:
+                        red = lightness;
+                        green = L0;
+                        blue = L1;
+                        break;
+                }
+            }
+
+            rgb[0] = (byte)(255.0 * red);
+            rgb[1] = (byte)(255.0 * green);
+            rgb[2] = (byte)(255.0 * blue);
+        }
 
         private void UninitializeKinectSensor(KinectSensor kinectSensor)
         {
